8f3e6022ffa95b6abab76d2cd2ac0ee918be8600
betalantz <betalantz@gmail.com>
2021-11-29 13:05:31 -0800

    implements dotenv to secure api key

--- public-api-fetch-demo/index.js
+++ public-api-fetch-demo/index.js
@@ -1,11 +1,13 @@
 // const fetch = require('node-fetch')
 import fetch from "node-fetch"
+import 'dotenv/config'
+
 
 fetch("https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=38.5&lat=-78.5", {
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com",
-		"x-rapidapi-key": "3d001f9d8emshad8baf330954b72p18c7d7jsnd54d7284750d"
+		"x-rapidapi-host": process.env.X_RAPIDAPI_HOST,
+		"x-rapidapi-key": process.env.X_RAPIDAPI_KEY
 	}
 })
 .then(response => response.json())

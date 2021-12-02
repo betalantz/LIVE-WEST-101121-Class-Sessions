74c8bc99e57f6f82fab56856e50585ad7fddef7a
betalantz <betalantz@gmail.com>
2021-12-02 10:59:53 -0800

    adds more performant solution for .most_popular

--- phase-3/phase-3-object-relations-practice-code-challenge-magazines-many-many/app/models/magazine.rb
+++ phase-3/phase-3-object-relations-practice-code-challenge-magazines-many-many/app/models/magazine.rb
@@ -1,7 +1,10 @@
 class Magazine < ActiveRecord::Base
 
     def self.most_popular
-        self.all.max{|a, b| a.subscriptions.length <=> b.subscriptions.length }
+        # self.all.max{|a, b| a.subscriptions.length <=> b.subscriptions.length }
+        subs_arr = Subscription.all.map{|sub| sub.magazine_id} # this solution is more performant than above
+        target_id = subs_arr.max_by {|sub| subs_arr.count(sub)}
+        self.find(target_id)
     end
 
     has_many :subscriptions

4aeba57171ecfe6197088e9ff7f137d80860f27e
betalantz <betalantz@gmail.com>
2021-12-17 15:38:58 -0800

    cleans up Camping

--- phase-4/phase-4-mock-challenge-camping-fun/app/controllers/signups_controller.rb
+++ phase-4/phase-4-mock-challenge-camping-fun/app/controllers/signups_controller.rb
@@ -1,9 +1,5 @@
 class SignupsController < ApplicationController
 
-    # def index
-    #     render json: Signup.all
-    # end
-
     def create
         signup = Signup.create!(signup_params)
         render json: signup.activity, status: :created

a6fbe6536587e37cfbdf341f6eb5bb0fcea9807b
betalantz <betalantz@gmail.com>
2021-12-17 11:27:30 -0800

    all bonus deliverables for Gym

--- phase-4/phase-4-rails-practice-challenge-gym/app/controllers/clients_controller.rb
+++ phase-4/phase-4-rails-practice-challenge-gym/app/controllers/clients_controller.rb
@@ -2,15 +2,24 @@ class ClientsController < ApplicationController
 
     before_action :find_client, only: [:show, :update]
 
+    def index
+        render json: Client.all
+    end
+
     def show
         render json: @client, serializer: TotalMembershipsSerializer
     end
 
     def update
-
+        @client.update!(client_params)
+        render json: @client
     end
 
     private
+
+    def client_params
+        params.permit(:name, :age)
+    end
 
     def find_client
         @client = Client.find(params[:id])
